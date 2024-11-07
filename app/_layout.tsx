321d3abb1215cba891b3f166a5f93f5c56f1caff
frashid17 <frashid274@gmail.com>
2024-11-07 17:17:58 +0300

    modified the layout coloring

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,9 @@
 import { Stack } from "expo-router";
-import { StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
   return (
-    <View style={styles.rootContainer}>
-      <Stack>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="job-listings" />
-        <Stack.Screen name="signup" />
-      </Stack>
-    </View>
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
   );
 }
-
-const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    backgroundColor: "#f4f4f9",
-  },
-});
