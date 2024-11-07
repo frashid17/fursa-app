7865697ebdd57b41ff7c715a7b58708465074020
frashid17 <frashid274@gmail.com>
2024-11-07 14:15:34 +0300

    exported root layout function

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,21 @@
 import { Stack } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <View style={styles.rootContainer}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="job-listings" />
+        <Stack.Screen name="signup" />
+      </Stack>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    backgroundColor: "#f4f4f9",
+  },
+});
