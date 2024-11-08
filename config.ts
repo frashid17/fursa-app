9d77883814fc1169c12fd5d83cf4db94d167a80f
frashid17 <frashid274@gmail.com>
2024-11-08 14:26:01 +0300

    Determined local network IP address

--- config.ts
+++ config.ts
@@ -1,1 +1,1 @@
-export const API_URL = "http://192.168.1.199:8000/api";  // Your Django API URL using the local IP address
+export const API_URL = "http://10.0.1.215:8000/api";  // Your Django API URL using the local IP address
