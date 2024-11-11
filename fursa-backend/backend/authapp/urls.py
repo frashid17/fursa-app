2cb7bf8c995b181227e1fc13e31018bf276ad6cb
frashid17 <frashid274@gmail.com>
2024-11-11 15:23:06 +0300

    added URL for contact form in backend URLs

--- fursa-backend/backend/authapp/urls.py
+++ fursa-backend/backend/authapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignupView, LoginView
+from .views import SignupView, LoginView,ContactUsView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+     path('contact/', ContactUsView.as_view(), name='contact_us'),
 ]
