5361c06809f060bcb99442f318a8659df839b48f
frashid17 <frashid274@gmail.com>
2024-11-13 16:46:49 +0300

    added job model url

--- fursa-backend/backend/authapp/urls.py
+++ fursa-backend/backend/authapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignupView, LoginView,ContactUsView
+from .views import SignupView, LoginView,ContactUsView, JobListView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
-     path('contact/', ContactUsView.as_view(), name='contact_us'),
+    path('contact/', ContactUsView.as_view(), name='contact_us'),
+    path('api/jobs/', JobListView.as_view(), name='job-list'),
 ]
