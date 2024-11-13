cc09e80c9ce0297afd20d9adb5567dfdb4937178
frashid17 <frashid274@gmail.com>
2024-11-13 16:45:13 +0300

    created a new model for jobs

--- fursa-backend/backend/authapp/models.py
+++ fursa-backend/backend/authapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+    requirements = models.TextField()
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
